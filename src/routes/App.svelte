1b1c12e5df6a46032e5e692e3d6f4d05a84d5a82
Vivek <vs690799@gmail.com>
2024-08-04 02:58:18 +0530

    added new component

---
+++ src/routes/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	let markdown = 'Write something here...';
+	let html = '';
+
+	// $: console.log(markdown);
+</script>
+
+<section class="flex justify-between text-slate-300 mt-4 mx-2">
+	<section class="w-6/12">
+		<textarea bind:value={markdown} rows="30" name="text" class="bg-slate-950 w-full"></textarea>
+	</section>
+	<section class="w-5/12">preview</section>
+</section>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: rgb(20, 20, 32);
+	}
+</style>
